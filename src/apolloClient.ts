ad2cb91ee411ae142bc361c6cb51f8deadc4640e
Leo Urzua <d.leonardo.urzua@gmail.com>
2023-04-06 00:41:19 +0200

    update graphQL endpoint to use the remote

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: 'https://ua6nzlb109.execute-api.us-east-1.amazonaws.com',
   cache: new InMemoryCache(),
 });
 
